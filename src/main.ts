554c46104dbb214ee96c66272836650846dac686
Ethan Lin <eragon9356@gmail.com>
2024-11-02 23:06:57 -0700

    Finished L16 Dialog

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-// todo
+const button = document.createElement("button");
+button.innerHTML = "Click Me";
+
+button.addEventListener("click", () => {
+  alert("You clicked the button!");
+});
+
+document.body.append(button);
